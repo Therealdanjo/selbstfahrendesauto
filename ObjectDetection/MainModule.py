1f69ebab53d43b23c5659feb660a9369f1be4aaa
Therealdanjo <daniele.guidotti@outlook.it>
2021-04-19 15:01:20 +0200

    feat: start playing sound if a person, cat, dog or bird is detected

--- ObjectDetection/MainModule.py
+++ ObjectDetection/MainModule.py
@@ -1,12 +1,24 @@
 from ObjectDetectorModule import *
+from pygame import mixer
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
 #cap.set(10,70)
+
+#intialize audio mixer and load audio
+mixer.init()
+channel = mixer.Channel(5)
+sound = mixer.Sound("CarAlarm.wav")
+
 while True:
     success,img = cap.read()
-    result, objectInfo = getObjects(img, 0.45, 0.5, True, ['person', 'cat', 'dog', 'bird'])
-    # print(objectInfo)
-    cv2.imshow("Output",img)
+    result, objectInfo = getObjects(img, 0.45, 0.2, True, ['person', 'cat', 'dog', 'bird'])
+    for found in objectInfo:
+        for fields in found:
+            if fields == 'person' or fields == 'cat' or fields == 'dog' or fields == 'bird':
+                if not channel.get_busy():
+                    channel.play(sound)
+
+    cv2.imshow("Output", img)
     cv2.waitKey(1)
