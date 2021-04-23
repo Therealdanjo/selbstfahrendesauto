924826f3816efb8be8419af9f5c635c23710ad36
Therealdanjo <daniele.guidotti@outlook.it>
2021-04-23 10:41:58 +0200

    feat: added parameters configurable via program arguments feat: added LED Code fix: mad CarAlarm.wav louder and shorter

--- ObjectDetection/ObjectDetectorModule.py
+++ ObjectDetection/ObjectDetectorModule.py
@@ -2,25 +2,26 @@ import cv2
 
 # Required OpenCV 4.4 and upwards on a raspberrypi
 
-thres = 0.45 # Threshold to detect object -> At least this percent to be recognized
+thres = 0.45    # Threshold to detect object -> At least this percent to be recognized
 
-classNames= []
+classNames = []
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
 
 #Complete path so that the raspberry can run it -> Not needed on other OSs
-configPath = '/home/pi/ObjectDetection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightsPath = '/home/pi/ObjectDetection/frozen_inference_graph.pb'
+configPath = '/home/pi/selbstfahrendesauto/ObjectDetection/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = '/home/pi/selbstfahrendesauto/ObjectDetection/frozen_inference_graph.pb'
 
 net = cv2.dnn_DetectionModel(weightsPath,configPath)
-net.setInputSize(320,320)
-net.setInputScale(1.0/ 127.5)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-def getObjects(img,thres, nms, draw=True, objects=[]):    
-    classIds, confs, bbox = net.detect(img,confThreshold=thres, nmsThreshold=nms)
+
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nms)
     #print(classIds,bbox)
     if len(objects) == 0:
         objects = classNames
@@ -33,22 +34,22 @@ def getObjects(img,thres, nms, draw=True, objects=[]):
             if className in objects:
                 objectInfo.append([box, className])
                 if(draw):
-                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-                    cv2.putText(img,className.upper(),(box[0]+10,box[1]+30),
-                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
-                            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                    cv2.putText(img, className.upper(), (box[0]+10, box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    cv2.putText(img, str(round(confidence*100, 2)), (box[0]+200, box[1]+30),
+                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
             
-    return img,objectInfo 
-    
+    return img, objectInfo
+
+
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
-    cap.set(3,640)
-    cap.set(4,480)
-    #cap.set(10,70)
+    cap.set(3, 640)
+    cap.set(4, 480)
     while True:
-        success,img = cap.read()
-        result, objectInfo = getObjects(img)
+        success, img = cap.read()
+        result, objectInfo = getObjects(img, 0.45, 0.2)
         print(objectInfo)
-        cv2.imshow("Output",img)
+        cv2.imshow("Output", img)
         cv2.waitKey(1)
